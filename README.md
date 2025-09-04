b540e86d94eb078ccd339d185098b2960aac33ee
klzw2233 <jimmyliubus2020@gmail.com>
2025-09-04 08:21:35 +0800

    use Constants to set Dynamos input output.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Infinite Power for Gregtech6
+
+### Powerful Dynamos added
+
+## Goal For Development
+
+### Cooler
+
+* Laval Nozzle, use GT6 SU to produce CU
+* Air Conditioning Compressor, use KU/RU to produce CU
+
--- src/main/java/com/klzw2233/infinitepower/core/ModConstants.java
+++ src/main/java/com/klzw2233/infinitepower/core/ModConstants.java
@@ -5,4 +5,9 @@ public class ModConstants {
     // 代理路径
     public static final String CLIENT_PROXY = "com.klzw2233.infinitepower.proxy.ClientProxy";
     public static final String COMMON_PROXY = "com.klzw2233.infinitepower.proxy.CommonProxy";
+
+    // 机器等级对应的输入输出
+    public static final int[] MInput = {16, 32, 64, 128, 256, 512};
+
+    public static final int[] MOutput = {256, 1024, 4096, 16384, 65536, 262144};
 }
